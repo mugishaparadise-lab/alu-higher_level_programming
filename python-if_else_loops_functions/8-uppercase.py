6e8cc0d36ad17f241ab3496c09f2b6a48706c661
mugishaparadise-lab <m.paradise@gmail.com>
2025-10-17 09:23:36 -0700

    errors bye

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
 def uppercase(str):
+    result = ""
     for c in str:
-        if 'a' <= c <= 'z':
-            print("{}".format(chr(ord(c) - 32)), end="")
+        # If lowercase letter
+        if 97 <= ord(c) <= 122:
+            # Convert to uppercase by subtracting 32
+            result += chr(ord(c) - 32)
         else:
-            print("{}".format(c), end="")
-    print()
+            result += c
+    print("{}".format(result))
